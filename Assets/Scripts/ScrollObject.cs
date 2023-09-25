cd618682bb16bdea60fc5bde05000eb5f0206f70
ruifukuda <rufk222@gmail.com>
2023-09-25 13:32:48 +0900

    スクリプトPlayerController,ScrollObject追加

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startPosition;
+    public float endPosition;
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x <= endPosition) ScrollEnd();
+    }
+
+    void ScrollEnd(){
+        float diff = transform.position.x - endPosition;
+        Vector3 restartPosition = transform.position;
+        restartPosition.x = startPosition + diff;
+        transform.position = restartPosition;
+
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+    }
+}
