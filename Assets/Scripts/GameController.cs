f4739c502f3470943b6cfceba1f605adb08c5b03
io <kapi.name.pooh@gmail.com>
2023-09-27 14:38:24 +0900

    スクリプト調整TitleからGameにつなげるように調整*後で変える

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class Gamecontroller : MonoBehaviour
 {
-    public PlayerController PL;
+    public PlayerController pl;
     public LifePanel lifePanel;
     void Update()
     {
+        lifePanel.UpdateLife(pl.life);
+        if(pl.life <= 0)
+        {
+            enabled = false;
+            Invoke("ReturunToTitle",2.0f);
+        }
+    }
+    void ReturnTotile()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
