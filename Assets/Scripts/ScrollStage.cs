6048a477ab510dc5fa8a2c99fd4814a5ec3cf5b8
ruifukuda <rufk222@gmail.com>
2023-09-28 11:00:04 +0900

    スクリプト修正

--- Assets/Scripts/ScrollStage.cs
+++ Assets/Scripts/ScrollStage.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class ScrollStage : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public float startPosition;
-    public float endPosition;
+    public float speed = 8.0f;
     void Update()
     {
         transform.Translate(-1 * speed * Time.deltaTime,0,0);
-        if(transform.position.x <= endPosition)
+        if(transform.position.x <= -20)
         {
             Destroy(gameObject);
         }
