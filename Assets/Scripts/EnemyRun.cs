afd5211c1b54c18a48e3914858da058726be83ea
ruifukuda <rufk222@gmail.com>
2023-10-03 11:06:18 +0900

    スクリプト修正

--- Assets/Scripts/EnemyRun.cs
+++ Assets/Scripts/EnemyRun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyRun : MonoBehaviour
 {
-    public float speed = 1.0f;
+    [Header("敵が向かってくる速度")] public float speed = 1.0f;
     void Update()
     {
         if(transform.position.x <= 15)
