523c45783a2fce8708743e4278d74d71ec5e351c
io <kapi.name.pooh@gmail.com>
2023-10-04 12:56:21 +0900

    総本山統合のためcommit

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -1,19 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Crash : MonoBehaviour
 {
-    Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    public GameObject gameObjectToDisable;
+    public GameObject gameObjectToEnable;
+
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+    void ReturnToTitleWithoutAnimation()
+    {
+        //タイトルシーンに移行
+        SceneManager.LoadScene("TitleCo");
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Truck")
+        {
+            ReturnToTitleWithoutAnimation();//DeathPointで死んだときvoid ReturnToTitleを呼び出す
+        }
+    }
+    //GameObjectを無効にするメソッド
+    void DisableGameObject()
+    {
+        if(gameObjectToDisable != null)
+        {
+            gameObjectToDisable.SetActive(false);
+        }
+        if(gameObjectToEnable != null)
+        {
+            gameObjectToEnable.SetActive(true);
+        }
+    }
+    //GameObjectを有効にするメソッド
+    void EnableGameObject()
+    {
+        if(gameObjectToDisable != null)
+        {
+            gameObjectToDisable.SetActive(true);
+        }
+        if(gameObjectToEnable != null)
+        {
+            gameObjectToEnable.SetActive(false);
+        }
     }
 }
