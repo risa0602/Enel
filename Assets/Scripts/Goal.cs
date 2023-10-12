bfb00d5218f37443ba0bb9c0b12ac4d1386927b2
io <kapi.name.pooh@gmail.com>
2023-10-12 13:28:32 +0900

    ボタン画像

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,9 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 衝突したオブジェクトが "Goal" タグを持つ場合
+            // "ScrollBackground" スクリプトを持つオブジェクトを停止
+            
+            Debug.Log("ok");
+            ScrollBackground[] scrollBackgrounds = FindObjectsOfType<ScrollBackground>();
+            foreach (ScrollBackground scrollBackground in scrollBackgrounds)
+            {
+                scrollBackground.StopScrolling();
+            }
 
+            // "ScrollStage" スクリプトを持つオブジェクトを停止
+            ScrollStage[] scrollStages = FindObjectsOfType<ScrollStage>();
+            foreach (ScrollStage scrollStage in scrollStages)
+            {
+                scrollStage.StopScrolling();
+            }
+    }
 }
--- Assets/Scripts/ScrollStage.cs
+++ Assets/Scripts/ScrollStage.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class ScrollStage : MonoBehaviour
 {
     public float speed = 8.0f;
+    bool isScrolling = true;
+
     void Update()
     {
-        transform.Translate(-1 * speed * Time.deltaTime,0,0);
-        if(transform.position.x <= -40)
+        if (isScrolling)
         {
-            Destroy(gameObject);
+            transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+            if (transform.position.x <= -40)
+            {
+                Destroy(gameObject);
+            }
+            if (transform.position.y <= -60)
+            {
+                Destroy(gameObject);
+            }
         }
-        if(transform.position.y <= -60)
-        {
-            Destroy(gameObject);
-        }
+    }
+
+    public void StopScrolling()
+    {
+        isScrolling = false;
     }
 }
